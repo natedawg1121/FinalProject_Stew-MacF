e70e85399fd06bcaefb451d1a4cf26a4aa097ede
Timothy MacFarlane <macfartc@mail.uc.edu>
2021-12-01 16:16:02 -0500

    Done

--- FinalProject_Stew-MacF/GroupSchool.cs
+++ FinalProject_Stew-MacF/GroupSchool.cs
@@ -8,5 +8,13 @@ namespace FinalProject_Stew_MacF
     public class GroupSchool
     {
         public string Name { get; set; }
+
+        public string SchoolCollege { get; set; }
+
+        public string SchoolHigh { get; set; }
+
+        public string SchoolMiddle { get; set; }
+
+        public string SchoolElementary { get; set; }
     }
 }
