38c8c2a716eb43beb84f82c3f878b7594a941010
Nathanous Trawets <natedawg1121@gmail.com>
2021-12-06 19:19:27 -0500

    made small changes for nswag

--- FinalProject_Stew-MacF/Controllers/GroupInformationsController.cs
+++ FinalProject_Stew-MacF/Controllers/GroupInformationsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FinalProject_Stew_MacF;
+using System.Linq.Dynamic.Core;
 
 namespace FinalProject_Stew_MacF.Controllers
 {
